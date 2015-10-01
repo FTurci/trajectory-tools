ce2dac0b87639e33a2c1e9d32a89b8b8a78afe3d
Joshua Robinson <jr15809@bristol.ac.uk>
2015-10-01 20:07:13 +0100

    Suggestions for argument parsing in comments. Also: cosmetic changes.

--- neighcorr.cpp
+++ neighcorr.cpp
@@ -2,25 +2,32 @@
 using namespace std;
 
 
+// I suggest usage: neighcorr $(ls -v path/to/files)
+// Unless we have particular need for specifying the first/last names, in which case boost.program_options is a good choice. This would require linking to boost though...
 int main(int argc, char const *argv[])
 {
-    if(argc<5) {cerr<<"Mandatory arguments: path to files, first frame, last frame, output name"<<endl; exit(0);}
-    int first=atoi(argv[2]);
-    int last=atoi(argv[3]);
-    string output_file=argv[4];
-
+    if (argc < 5)
+    {
+        cerr << "Mandatory arguments: path to files, first frame, last frame, output name" << endl;
+        exit(0);
+    }
+    
+    int first = StringToNum<int> (argv[2]);
+    int last = StringToNum<int>(argv[3]);
+    string output_file = argv[4];
+    
     trajectory Trajectory;
     
-    cout<<"Reading data..."<<endl;
-
-    Trajectory.read_sequence(argv[1],first , last);
-
-    cout<<"The trajectory length is "<<Trajectory.length()<<endl;
-
+    cout << "Reading data..." << endl;
+    
+    Trajectory.read_sequence(argv[1], first, last);
+    
+    cout << "The trajectory length is " << Trajectory.length() << endl;
+    
     bool sorting=false;
     Trajectory.compute_neighbour_correlation(sorting);
     Trajectory.save_neighbour_correlation(output_file);
-
+    
     return 0;
 }
 
